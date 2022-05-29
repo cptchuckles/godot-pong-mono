128e208509d55b88ef5bca1191e315d51870fe80
Grendel <jcg@grendel.world>
2022-05-29 15:26:15 -0500

    Elide PositionResetter.cs
    Use good old inheritance instead of awkward composition.

--- objects/Paddle/Paddle.cs
+++ objects/Paddle/Paddle.cs
@@ -5,4 +5,17 @@ public class Paddle : KinematicBody2D
 {
 	[Export]
 	protected float _speed = 200f;
+
+	private Vector2 _startingPosition;
+
+	public override void _Ready()
+	{
+		_startingPosition = GlobalPosition;
+		GetNode("/root/EventBus").Connect("PuckSpawned", this, "OnPuckSpawned");
+	}
+
+	private void OnPuckSpawned(Puck puck)
+	{
+		GlobalPosition = new Vector2(_startingPosition.x, GlobalPosition.y);
+	}
 }
